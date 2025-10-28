d9fdc3676a9e6cf13c309e0f6eab83cbee587921
Janwrice <77104205+Janwrice@users.noreply.github.com>
2025-10-28 17:41:54 +0800

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+//使用示例：
+
+#include<iostream>
+#include"calc.hpp"
+int main(void){
+	//创建计算对象并运行交互计算函数：
+	Calc c;
+	c.run();
+	//一个静态函数测试用例：
+	//std::cout << Calc::calc("7.65*(6/7-(7*9)/2.36+3)-5/(((7)))+-1.657*-2.37");
+	return 0;
+}
